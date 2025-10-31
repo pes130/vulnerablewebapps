fbefd80f50e704619ca26f49b0f08e324f989b5e
Pablo <pablo.esteban.sanchez@gmail.com>
2025-10-31 08:07:46 +0100

    Permitir log poisoning mutillidae- corrección de errores

--- mutillidae/docker-entrypoint.sh
+++ mutillidae/docker-entrypoint.sh
@@ -5,13 +5,16 @@ set -e
 mkdir -p /var/log/apache2
 touch /var/log/apache2/mutillidae-access.log /var/log/apache2/mutillidae-error.log
 
-if [ ! -e "/var/log/apache2/access.log" ]; then
-  ln -s /var/log/apache2/mutillidae-access.log /var/log/apache2/access.log
+if [ -e "/var/log/apache2/access.log" ]; then
+  rm /var/log/apache2/access.log
 fi
 
-if [ ! -e "/var/log/apache2/error.log" ]; then
-  ln -s /var/log/apache2/mutillidae-error.log /var/log/apache2/error.log
+if [ -e "/var/log/apache2/error.log" ]; then
+  rm /var/log/apache2/error.log
 fi
+
+ln -s /var/log/apache2/mutillidae-access.log /var/log/apache2/access.log
+ln -s /var/log/apache2/mutillidae-error.log /var/log/apache2/error.log
 
 # logs legibles por www-data
 chown www-data:www-data /var/log/apache2 /var/log/apache2/* || true
